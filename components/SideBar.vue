1bb5a21c0c99c788a7e5ca0efcf2b7fca5980187
Xybron <ronaldnettey@yahoo.com>
2023-11-15 03:12:35 +0000

    Side bar dynamism added

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -8,36 +8,28 @@
             <div class="flex flex-col justify-between flex-1 mt-8">
                 <div class="space-y-4 flex-grow-0">
                     <nav class="flex-1 space-y-2">
-                        <a href="#" title="" class="flex items-center px-4 py-3 text-sm font-medium text-indigo-600 transition-all duration-200 bg-indigo-50 group">
+                        <NuxtLink to="/overview" title="" :class="{'bg-indigo-50 text-blue-600  mx-5 rounded-lg' : isActive('/overview')}" class="flex items-center px-4 py-3 text-sm font-medium transition-all duration-200  group">
                             <svg class="flex-shrink-0 w-5 h-5 mr-4" xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                             </svg>
                             Overview
-                        </a>
+                        </NuxtLink>
 
-                        <a href="#" title="" class="flex items-center px-4 py-3 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-200 group">
-                            <svg class="flex-shrink-0 w-5 h-5 mr-4" xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                            </svg>
+                        <NuxtLink to="/devices" title="" :class="{'bg-indigo-50 text-blue-600 mx-5 rounded-lg' : isActive('/devices')}" class="flex items-center px-4 py-3 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-200 group">
+                            <Icon name="material-symbols-light:device-hub-rounded" size="24" class="mr-5"></Icon>
                             Devices
-                        </a>
+                        </NuxtLink>
 
-                        <a href="#" title="" class="flex items-center px-4 py-3 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-200 group">
-                            <svg class="flex-shrink-0 w-5 h-5 mr-4" xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                            </svg>
+                        <NuxtLink to="/users" title="" :class="{'bg-indigo-50 text-blue-600  mx-5 rounded-lg' : isActive('/users')}" class="flex items-center px-4 py-3 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-200 group">
+                            <Icon name="fluent:people-12-regular" size="20" class="mr-5"></Icon>
                             Users
-                        </a>
+                        </NuxtLink>
 
-                        <a href="#" title="" class="flex items-center px-4 py-3 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-200 group">
-                            <svg class="flex-shrink-0 w-5 h-5 mr-4" xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-                            </svg>
+                        <NuxtLink to="/billing" :class="{'bg-indigo-50 text-blue-600  mx-5 rounded-lg' : isActive('/billing')}" title="" class="flex items-center px-4 py-3 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-200 group">
+                            <Icon name="streamline:money-cash-coins-stack-accounting-billing-payment-stack-cash-coins-currency-money-finance" size="15" class="mr-5"></Icon>
                             Billing
-                            <svg class="w-4 h-4 ml-auto text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-                            </svg>
-                        </a>
+                    
+                        </NuxtLink>
                     </nav>
                 </div>
                 <div class="px-5 pb-8 mt-16">
@@ -52,3 +44,7 @@
     </div>
 
 </template>
+<script setup lang="ts">
+console.log(useRoute().path)
+const isActive = (path:string)=> useRoute().path.startsWith(path)
+</script>
