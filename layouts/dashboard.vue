6622f5b136711891242d83b70de8ca6ded543497
Xybron <ronaldnettey@yahoo.com>
2023-11-15 13:46:02 +0000

    Hidden bottom nav on wide

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="w-screen h-screen overflow-hidden m-0 p-0 flex ">
     <SideBar></SideBar>
-    <section class="absolute z-40">
+    <section class="absolute z-40 md:hidden">
       <BottomNav></BottomNav>
     </section>
     <section class="flex-1 overflow-y-auto relative bg-pageBg">
