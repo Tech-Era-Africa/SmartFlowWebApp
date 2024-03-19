40aebebc2b87958eb8c1d05a8673913d87d68fc6
Xybron <ronaldnettey@yahoo.com>
2024-03-19 16:51:34 +0000

    added some meta to see if this will work

--- app.vue
+++ app.vue
@@ -1,11 +1,18 @@
 <template>
   <NuxtLoadingIndicator :height="5"></NuxtLoadingIndicator>
-    <NuxtPage></NuxtPage>
+  <NuxtPage></NuxtPage>
 </template>
 <script setup lang="ts">
 useHead({
   titleTemplate: (titleChunk) => {
     return titleChunk ? `${titleChunk} - SmartFlow` : 'SmartFlow';
-  }
+  },
+  meta: [
+    { hid: 'description', name: 'description', content: "Monitor your water consumption with ease." },
+    // Add more meta tags as needed
+    { hid: 'og:title', property: 'og:title', content: "Smart Flow | Dashboard" },
+    { hid: 'og:description', property: 'og:description', content: "Monitor your water consumption and devices with ease." },
+    { hid: 'og:image', property: 'og:image', content: '/logo.png' },
+  ]
 })
 </script>
