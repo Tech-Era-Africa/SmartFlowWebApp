29a6f6c387e51af8e69b96d3649d9f3472f90121
Xybron <ronaldnettey@yahoo.com>
2024-03-19 14:57:37 +0000

    Button added to send user back home

--- pages/org/new.vue
+++ pages/org/new.vue
@@ -1,9 +1,10 @@
 <template>
-    <section class="w-screen h-screen bg-cyan-50 flex justify-center items-center">
+    <section class="w-screen h-screen bg-cyan-50 flex flex-col gap-5 justify-center items-center">
         <p>Loading orgs...</p>
+        <Button @click="useRouter().replace('/')">Go Home</Button>
     </section>
 </template>
 <script setup lang="ts">
 useHead({ title: "Setup" })
-definePageMeta({ middleware: 'auth' })
+
 </script>
