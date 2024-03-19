74fc2eff7f820fc33350ec94d8a8c15b1089a665
Xybron <ronaldnettey@yahoo.com>
2024-03-19 20:02:13 +0000

    og update

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -9,3 +9,14 @@
     </section>
   </section>
 </template>
+<script setup lang="ts">
+useHead({
+  meta: [
+    { hid: 'description', name: 'description', content: "Monitor your water consumption with ease." },
+    // Add more meta tags as needed
+    { hid: 'og:title', property: 'og:title', content: "Smart Flow | Dashboard" },
+    { hid: 'og:description', property: 'og:description', content: "Monitor your water consumption and devices with ease." },
+    { hid: 'og:image', property: 'og:image', content: '/logo.png' },
+  ]
+})
+</script>
